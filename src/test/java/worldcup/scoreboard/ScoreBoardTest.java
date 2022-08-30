776b8ac044e6ad8f6f41cf036cf24406e9f8fecf
Constantin Golan <constantin.golan@yahoo.com>
2022-08-30 20:23:01 +0300

    Created test cases scenarios according to the requirements

---
+++ src/test/java/worldcup/scoreboard/ScoreBoardTest.java
@@ -0,0 +1,27 @@
+package src.test.java.worldcup.scoreboard;
+
+
+import org.junit.jupiter.api.Test;
+
+public class ScoreBoardTest {
+
+    @Test
+    void should_start_a_new_game(){
+
+    }
+
+    @Test
+    void should_finish_game_in_progress(){
+
+    }
+
+    @Test
+    void should_update_score(){
+
+    }
+
+    @Test
+    void should_display_summary_of_games_ordered_by_total_score_and_by_most_recently_started_game(){
+
+    }
+}
